5aa1ec2744a5835d1f267581d820db36b851e478
arthur <arthurfoadjo@yahoo.com>
2022-06-24 04:49:32 +0200

    first commit

---
+++ ApplicationAPI/Controllers/TutoController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TutoController : ControllerBase
+    {
+        Tuto[] tutos = new Tuto[]
+        {
+            new Tuto { title = "wpf", price = 49, image = "wpf.png", description = "Blalabla....." },
+            new Tuto { title = "Unity", price = 27, image = "unity.png", description = "Lalala....." },
+            new Tuto { title = "JavaScript", price = 49, image = "js.png", description = "Blobloblo....." },
+            new Tuto { title = "Java", price = 40, image = "java.png", description = "Lololo....." },
+            new Tuto { title = "DevOps", price = 23, image = "devo.png", description = "Buuuuzzzzz....." },
+            new Tuto { title = "web development", price = 50, image = "web.png", description = "Okkkkkkkkkk....." }
+        };
+
+        private readonly ILogger<TutoController> _logger;
+
+        public TutoController(ILogger<TutoController> logger)
+        {
+            _logger = logger;
+        }
+       
+        // Possible api calls
+
+        [HttpGet]
+        public IEnumerable<Tuto> Get()
+        {
+            return tutos;
+        }
+
+        [HttpGet]
+        [Route("index")]
+        public Tuto GetByIndex(int i = 0)
+        {
+            return tutos[i];
+        }
+    }
+}
---
+++ ApplicationAPI/Tuto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationAPI
+{
+    public class Tuto
+    {
+        public string title { set; get; }
+
+        public int price { set; get; }
+
+        public string image { set; get; }
+
+        public string description { set; get; }
+
+        public override string ToString()
+        {
+            return title + " | " + price;
+        }
+
+    }
+}
